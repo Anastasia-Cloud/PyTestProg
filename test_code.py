bf60511b92e27424477a1c1e28d4f6c4466cfe46
AnastasiaCloud <73059931+Anastasia-Cloud@users.noreply.github.com>
2021-09-24 11:56:30 +0000

    add tests

--- test_code.py
+++ test_code.py
@@ -6,22 +6,6 @@ import pytest
 def test_answer():
     assert func(3) == 4
 
-def f():
-    raise SystemExit(1)
-
-
-def test_mytest():
-    with pytest.raises(SystemExit):
-        f()
-
-class TestClass:
-    def test_one(self):
-        x = "this"
-        assert "h" in x
-
-    def test_two(self):
-        x = "hello"
-        assert hasattr(x, "check")
 
 
 @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
@@ -32,4 +16,41 @@ def test_eval(test_input, expected):
 @pytest.mark.parametrize("y", [2, 3])
 def test_foo(x, y):
     pass
+
 '''
+class TestClassSet:
+    def test_1(self):
+        set_1 = set('qwerty')
+        assert len(set_1) == 6
+
+    def test_2(self):
+        set_1 = set('')
+        try:
+          assert set_1.pop() == 6
+        except KeyError:
+          pass 
+    
+    @pytest.mark.parametrize("item", ['q','w','e','r','t','y'])
+    
+    def test_3(self,item):
+      set_1 = set('qwerty')
+      assert item in set_1
+  
+
+class TestClassDict:
+    def test_1(self):
+        dict_1 = dict(zip(["a", "b", "c"], [1,2,3]))
+        assert dict_1["a"] == 1
+
+    def test_2(self):
+        dict_1 = dict()
+        try:
+          assert dict_1["e"] == 333
+        except KeyError:
+          pass 
+    
+    @pytest.mark.parametrize("test_input,expected", [("a", 1), ("b", 2), ("c", 3)])
+    
+    def test_3(self, test_input, expected):
+      dict_1 = dict(zip(["a", "b", "c"], [1, 2, 3]))
+      assert dict_1[test_input] == expected
