99b71a88dc717a3964cf19ea8f8493e6e693459c
AnastasiaCloud <73059931+Anastasia-Cloud@users.noreply.github.com>
2021-09-24 11:57:08 +0000

    clear comments

--- test_code.py
+++ test_code.py
@@ -1,23 +1,4 @@
 import pytest
-'''def func(x):
-    return x + 1
-
-
-def test_answer():
-    assert func(3) == 4
-
-
-
-@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
-def test_eval(test_input, expected):
-    assert eval(test_input) == expected
-
-@pytest.mark.parametrize("x", [0, 1])
-@pytest.mark.parametrize("y", [2, 3])
-def test_foo(x, y):
-    pass
-
-'''
 class TestClassSet:
     def test_1(self):
         set_1 = set('qwerty')
